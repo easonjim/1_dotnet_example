ea17b67386ee1b61749150d427fd282f76207139
Jim <easonjim@163.com>
2016-08-31 18:38:12 +0800

    增加nuget安装的测试

---
+++ nuget/ConsoleApplication_InstallTest/ConsoleApplication_InstallTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using Easemob.Restfull4Net;
+using Easemob.Restfull4Net.Entity.Request;
+
+namespace ConsoleApplication_InstallTest
+{
+    class Program
+    {
+        private static string _userName
+        {
+            get
+            {
+                Thread.Sleep(100);
+                return DateTime.Now.ToString("yyyyMMddHHmmssffff");
+            }
+        } //用户名
+
+        static void Main(string[] args)
+        {
+            //单个创建
+            var syncRequest = Client.DefaultSyncRequest;
+
+            var user = syncRequest.UserCreate(new UserCreateReqeust()
+            {
+                nickname = string.Concat("Test", _userName),
+                password = "123456",
+                username = string.Concat("Test", _userName),
+            });
+            System.Console.Write((user.StatusCode == HttpStatusCode.OK).ToString());
+        }
+    }
+}
