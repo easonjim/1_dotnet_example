15c821e2828bb212766009d8cb02c4b095001ac3
Jim <easonjim@163.com>
2016-09-06 16:43:00 +0800

    增加强签名例子

---
+++ 密钥/ConsoleApplication_snk_Test/ClassLibrary/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Class1
+    {
+        public string GetName()
+        {
+            return "this is name";
+        }
+    }
+}
---
+++ 密钥/ConsoleApplication_snk_Test/ConsoleApplication_snk_Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication_snk_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(new ClassLibrary.Class1().GetName());
+        }
+    }
+}
